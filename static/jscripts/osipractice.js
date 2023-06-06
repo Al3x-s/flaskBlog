17c2730e6c0aed3716730b8908e2abc9771fb504
Alex <l3xpr1v@proton.me>
2023-06-06 17:25:34 -0500

    fixed JS type

--- static/jscripts/osipractice.js
+++ static/jscripts/osipractice.js
@@ -1,17 +1,15 @@
 
-x = 0;
 function logChoice() {
     // Get the selected radio button
     var selectedOption = document.querySelector('input[name="answer"]:checked');
     
     // Log the value of the selected radio button
     if (selectedOption) {
-        x = x +1; 
         console.log(x)
         console.log('Selected choice:', selectedOption.value);
     }
   }
-
-function run(br){
-    console.log(br)
-}
+function runny(){
+    var Option = document.querySelector('input[name="answer"]:checked');
+    console.log(yourmother)
+};
