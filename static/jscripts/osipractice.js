b7feac39b1b8a138b3116a64f683973be65b5b0d
Alex <l3xpr1v@proton.me>
2023-06-06 23:10:26 -0500

    fixed JS

--- static/jscripts/osipractice.js
+++ static/jscripts/osipractice.js
@@ -1,15 +1,53 @@
 
-function logChoice() {
+const person = {
+a:0,
+b:1,
+c:2,
+d:3
+};
+
+const person2 = {
+    0:'a',
+    1:'b',
+    2:'c',
+    3:'d'
+};
+
+    
+
+function logChoice(current) {
     // Get the selected radio button
     var selectedOption = document.querySelector('input[name="answer"]:checked');
-    
+    var cti = person[selectedOption.value]
+
     // Log the value of the selected radio button
     if (selectedOption) {
-        console.log(x)
-        console.log('Selected choice:', selectedOption.value);
+        // console.log(current.className)
+        console.log('******************************');
+        // console.log('correct to index', cti)
+        // console.log(yourmother[bb])
+        // console.log('correct answer:', selectedOption.value);
+        // console.log('-------------------------');
+        // console.log(yourmother[bb]['choices'][cti])
+        // console.log('||||||||||||||||||||||||||||||||||||||||||');
+
     }
   }
+
+
 function runny(){
-    var Option = document.querySelector('input[name="answer"]:checked');
-    console.log(yourmother)
+    
+    var selectedOption = document.querySelector('input[name="answer"]:checked');
+    var cti = person[selectedOption.value]
+    // console.log('asdasdasd',person[correctAnswer])
+
+    if(selectedOption.className == yourmother[bb]['choices'][cti]){
+        document.getElementById('isRight').innerHTML = 'Correct'
+        console.log(" Correct");
+        //console.log(person2[person[correctAnswer]])
+    }
+    if(selectedOption.className != yourmother[bb]['choices'][cti]){
+        document.getElementById('isRight').innerHTML = 'Wrong'
+        console.log("Noooooooooooooooo ")
+    }
 };
